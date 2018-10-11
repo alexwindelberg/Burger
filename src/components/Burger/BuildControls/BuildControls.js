82ad2de69c3c71fa7db6b11560283e8b5a6873ac
alexwindelberg <alexwindelberg@gmail.com>
2018-10-11 13:58:28 -0400

    added build controls

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import classes from './BuildControls.css';
+import BuildContol from './BuildControl/BuildControl';
+
+const controls = [
+    { label: 'Salad', type: 'salad'},
+    { label: 'Bacon', type: 'bacon'},
+    { label: 'Cheese', type: 'cheese'},
+    { label: 'Meat', type: 'meat '},
+];
 
 const buildControls = (props) => (
 
-    <div>
-          
+    <div className={classes.BuildControls}>
+          {controls.map(ctrl => (
+              <BuildContol key={ctrl.label} label={ctrl.label} />
+          ))}
     </div>
 
 );
--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -16,7 +16,7 @@ const burger = (props) => {
     console.log(transformedIngredients);
 
     if(transformedIngredients.length === 0 ) {
-        transformedIngredients = <p>Please start adding ingredients!</p>
+        transformedIngredients = <p> Please start adding ingredients! </p>
     }
 
     return (
