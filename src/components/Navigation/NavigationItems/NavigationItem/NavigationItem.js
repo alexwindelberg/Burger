2ad10a95da26caa4ba3d8249624c74dc0d9465d2
alexwindelberg <alexwindelberg@gmail.com>
2018-12-06 12:23:48 -0500

    working on NavLinks

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+
 
 import classes from './NavigationItem.css';
 
 const navigationItem = (props) => (
     <li className={classes.NavigationItem}>
-        <a href={props.link} 
-            className={props.active ? classes.active : null}>
+        {/* activeClassName helps fix the default selected item on the navbar but look more into it*/}
+        <NavLink to={props.link}
+                 exact = {props.exact}
+                 activeClassName={classes.active}> 
                 {props.children}
-        </a>
+        </NavLink>
     </li>
 );
 
