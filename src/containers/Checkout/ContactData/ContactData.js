ab8ed7683b2725a780763a06084eb388e518c126
alexwindelberg <alexwindelberg@gmail.com>
2018-12-11 18:13:25 -0500

    Adding Validation Feedback

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -159,7 +159,6 @@ class ContactData extends Component {
             formElementsArray.push({
                 id: key,
                 config: this.state.orderForm[key],
-
             });
         }
 
@@ -171,6 +170,8 @@ class ContactData extends Component {
                     elementType={formElement.config.elementType}
                     elementConfig={formElement.config.elementConfig}
                     value={formElement.config.value}
+                    invalid={!formElement.config.valid}
+                    shouldValidate={formElement.config.validation}
                     changed={(event) => this.inputChangedHandler(event, formElement.id)} />
             ))}
             <Button btnType="Success" clicked={this.orderHandler}>Order</Button>
